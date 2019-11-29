fd3a13fda5d1c0f58bfd2c3d09484a3f922ffd39
bond <bond@simicon.com>
2019-11-29 19:09:15 +0300

    show version

--- main2.cpp
+++ main2.cpp
@@ -16,6 +16,38 @@
 
 using namespace Wt;
 
+std::string readVersion()
+{
+	FILE *fp = fopen("/etc/version", "rt");
+	if (!fp) {
+		perror("can't read /etc/version : ");
+		return "";
+	}
+	std::string version;
+	char buf[4096];
+	while (!feof(fp)) {
+		buf[0] = 0;
+		buf[sizeof(buf) - 1] = 0;
+		fgets(buf, sizeof(buf) - 1, fp);
+		std::string line(buf);
+		std::string::size_type nlnpos = line.find_last_not_of("\n");
+		line.erase(nlnpos + 1);
+
+		size_t pos = line.find('=');
+		if (pos == std::string::npos)
+			continue;
+		std::string name = line.substr(0, pos);
+		std::string value = line.substr(pos + 1, std::string::npos);
+		if (name == "VERSION") {
+			version = value;
+			break;
+		}
+//      printf("\"%s\" = \"%s\"\n", name.c_str(), value.c_str() );
+	}
+	fclose(fp);
+	return version;
+}
+
 WApplication *createApplication(const WEnvironment &env)
 {
 	WApplication *app = new WApplication(env);
@@ -24,7 +56,8 @@ WApplication *createApplication(const WEnvironment &env)
 
 	std::string sdir = "";
 	app->root()->addWidget(new LocalGui(sdir));
-	app->setTitle("rigdo control pannel");
+	std::string version = readVersion();
+	app->setTitle(WString::tr("rigdo control pannel").arg(version));
 	app->useStyleSheet("css/style.css");
 	return app;
 }
